170ee1bbc6cb85a64c7f9d7871e60f03bdf38fce
Michael Reynolds <reynoldsmichael945@gmail.com>
2020-09-02 00:37:49 -0400

    Added addtional validation preventing crashing when adding jobs

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,20 @@
+//var eventNameLength = $( "#name_of_event" ).length;
+
+//$(document).ready(function() {
+//    $("#specificPeople").on("change", function(event) {
+//        $("#test2").toggle();
+//    });
+//});
+//
+//$(document).ready(function() {
+//	// get current URL path and assign 'active' class
+//	var pathname = window.location.pathname;
+//	$('#navBar > li > a[href="'+pathname+'"]').parent().addClass('active');
+//})
+
+//$(document).ready(function() {
+//
+//
+//getElementById("myBtn").disabled = true;
+//
+//}
